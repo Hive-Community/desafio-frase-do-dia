3f20fd765c1dc2a63070d5070f5f4f185bea8daf
z4nder <gusttavo212@gmail.com>
2023-08-02 12:16:44 -0300

    finalizado

---
+++ README.md
@@ -0,0 +1,8 @@
+### Desafio para testar aprendizados em JS
+
+Esse desafio consiste em pegarmos uma frase aleatória de um array de frases e apresentar para o usuário na tela. O foco desse teste é usar o JS, então o HTMl deve ser ignorado somente prestando atenção nos ids que vamos precisar usar id="frase-do-dia" para inserir a frase na tela e id="button-gerar-frase" que é o botão que vai gerar as frases.
+
+### Conhecimentos treinados
+
+- Arrays
+- Modificar HTML com js
---
+++ main.js
@@ -0,0 +1,12 @@
+const frases = [
+  "A persistência é o caminho do êxito.",
+  "O sucesso nasce do querer, da determinação e persistência em se chegar a um objetivo. Mesmo não atingindo o alvo, quem busca e vence obstáculos, no mínimo fará coisas admiráveis.",
+  "Lute. Acredite. Conquiste. Perca. Deseje. Espere. Alcance. Invada. Caia. Seja tudo o quiser ser, mas, acima de tudo, seja você sempre.",
+  "Agir, eis a inteligência verdadeira. Serei o que quiser. Mas tenho que querer o que for. O êxito está em ter êxito, e não em ter condições de êxito. Condições de palácio tem qualquer terra larga, mas onde estará o palácio se não o fizerem ali?",
+  "Determinação, coragem e autoconfiança são fatores decisivos para o sucesso. Se estamos possuídos por uma inabalável determinação, conseguiremos superá-los. Independentemente das circunstâncias, devemos ser sempre humildes, recatados e despidos de orgulho.",
+  "Só existe um êxito: a capacidade de levar a vida que se quer.",
+];
+
+function gerarFreaseDoDia() {
+  // return sua frase do dia
+}
